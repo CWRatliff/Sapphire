cdfcf9d1876e600dd842b052e25423eb63d0e14b
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2021-04-14 07:41:09 -0700

    Add files via upload

--- Sapphire.cpp
+++ Sapphire.cpp
@@ -17,7 +17,7 @@
 #include "RDbf.h"
 #include "RSapphire.h"
 
-//	errno_t	err;			
+	errno_t	err;			
 
 Sapphire::Sapphire() {
 	dbDbfRoot = NULL;
@@ -152,8 +152,7 @@ RDbf*	Sapphire::DbCreateFile(const char *dbfname) {
 
 	dbf = new RDbf(dbfname);
 	rc = dbf->Create(dbfname);
-//	if (rc == NULL) {
-	if (rc == 0) {
+	if (rc == NULL) {
 		delete dbf;
 		return (NULL);
 	}
@@ -166,5 +165,5 @@ RDbf*	Sapphire::DbCreateFile(const char *dbfname) {
 //==============================================================================
 // return system error number "errno"
 int Sapphire::DbGetErrno() {
-	return errno;
+	return err;
 	}
